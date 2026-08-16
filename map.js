6c7da3b4f4b0b78bb5cc679944bea0e7dc8f5d7e
smsiam114 <smsiamislam598@gmail.com>
2026-08-16 19:45:21 +0600

    find a new array with map

--- map.js
+++ map.js
@@ -8,4 +8,12 @@ const names = students.map((student) => {
     return student.name;
 });
 
-console.log(names);
+// console.log(names);
+
+const numbers = [5, 10, 15, 20, 25];
+
+const squareNumbers = numbers.map((number)=>{
+    return number * number;
+})
+
+console.log(squareNumbers);
